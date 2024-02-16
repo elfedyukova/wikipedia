ffc1530215bf1703f6872e35066c912171397ffc
elfedyukova <elfedyukova@yandex.ru>
2024-02-16 13:10:55 +0300

    add test 3

--- src/test/java/org/example/MainClassTest.java
+++ src/test/java/org/example/MainClassTest.java
@@ -4,9 +4,11 @@ import org.junit.Assert;
 import org.junit.Test;
 
 public class MainClassTest {
+
+    MainClass mainClass = new MainClass();
+
     @Test
     public void testGetLocalNumber() {
-        MainClass mainClass = new MainClass();
         int expected = 14;
         int actual = mainClass.getLocalNumber();
         Assert.assertTrue("Метод getLocalNumber не возвращает число 14!", expected == actual);
@@ -14,9 +16,14 @@ public class MainClassTest {
 
     @Test
     public void testGetClassNumber() {
-        MainClass mainClass = new MainClass();
         int expected = 45;
         int actual = mainClass.getClassNumber();
         Assert.assertTrue("Метод getClassNumber возвращает число меньше или равное 45!", actual > expected);
+    }
+
+    @Test
+    public void testGetClassString() {
+        String actual = mainClass.getClassString();
+        Assert.assertTrue("Метод getClassString возвращает строку без подстрок 'hello' и 'Hello'!", actual.contains("hello") || actual.contains("Hello"));
     }
 }
