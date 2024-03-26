e9c7c9f2e2e046dc1a7b332a73452e6312d32e7c
elfedyukova <elfedyukova@yandex.ru>
2024-03-26 17:46:14 +0300

    refactor tests

--- src/test/java/org/example/SimpleTest.java
+++ src/test/java/org/example/SimpleTest.java
@@ -1,18 +1,26 @@
 package org.example;
 
 import io.appium.java_client.AppiumDriver;
+import io.appium.java_client.TouchAction;
 import io.appium.java_client.android.AndroidDriver;
+import io.appium.java_client.touch.WaitOptions;
+import io.appium.java_client.touch.offset.PointOption;
 import org.junit.After;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import org.openqa.selenium.By;
+import org.openqa.selenium.Dimension;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.PointerInput;
+import org.openqa.selenium.interactions.Sequence;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.net.URL;
+import java.time.Duration;
+import java.util.Arrays;
 
 public class SimpleTest {
 
@@ -42,14 +50,14 @@ public class SimpleTest {
     @Test
     public void elementHasTextTest() {
 
-        waitForElementByIdAndClick(
-                "org.wikipedia:id/fragment_onboarding_skip_button",
+        waitForElementPresentAndClick(
+                By.id("org.wikipedia:id/fragment_onboarding_skip_button"),
                 "Cannot find 'Skip element' ",
                 5
         );
 
-        waitForElementByXpathAndClick(
-                "//*[contains(@text,'Search Wikipedia')]",
+        waitForElementPresentAndClick(
+                By.xpath("//*[contains(@text,'Search Wikipedia')]"),
                 "Cannot find 'Search element' ",
                 5
         );
@@ -65,35 +73,32 @@ public class SimpleTest {
     @Test
     public void searchCancelTest() {
 
-        waitForElementByIdAndClick(
-                "org.wikipedia:id/fragment_onboarding_skip_button",
+        waitForElementPresentAndClick(
+                By.id("org.wikipedia:id/fragment_onboarding_skip_button"),
                 "Cannot find 'Skip element' ",
                 5
         );
 
-        waitForElementByXpathAndClick(
-                "//*[contains(@text,'Search Wikipedia')]",
+        waitForElementPresentAndClick(
+                By.xpath("//*[contains(@text,'Search Wikipedia')]"),
                 "Cannot find 'Search element' ",
                 5
         );
 
-        waitForElementByXpathAndSendKeys(
-                "//*[contains(@text,'Search Wikipedia')]",
+        waitForElementPresentAndSendKeys(
+                By.xpath("//*[contains(@text,'Search Wikipedia')]"),
                 "Java",
                 "Cannot find 'Search element' ",
                 5
         );
-        //  Было By.id("org.wikipedia:id/search_results_list")
-        //  Пробовала By.xpath("android.widget.TextView[contains(@text, 'Java')]") и то, что сейчас в тесте Почему-то не проходит тест
-        // By.xpath("android.widget.TextView@resource-id=\"org.wikipedia:id/page_list_item_title\" and contains(@text, 'Java')")
+        //
         waitForElementPresent(
                 By.id("org.wikipedia:id/search_results_list"),
-                "Cannot find 'element' ",
-                5
+                "Cannot find 'element' "
         );
 
-        waitForElementByIdAndClick(
-                "org.wikipedia:id/search_close_btn",
+        waitForElementPresentAndClick(
+                By.id("org.wikipedia:id/search_close_btn"),
                 "Cannot find 'Skip element' ",
                 5
         );
@@ -108,20 +113,20 @@ public class SimpleTest {
     @Test
     public void wordSearchTest() {
 
-        waitForElementByIdAndClick(
-                "org.wikipedia:id/fragment_onboarding_skip_button",
+        waitForElementPresentAndClick(
+                By.id("org.wikipedia:id/fragment_onboarding_skip_button"),
                 "Cannot find 'Skip element' ",
                 5
         );
 
-        waitForElementByXpathAndClick(
-                "//*[contains(@text,'Search Wikipedia')]",
+        waitForElementPresentAndClick(
+                By.xpath("//*[contains(@text,'Search Wikipedia')]"),
                 "Cannot find 'Search element' ",
                 5
         );
 
-        waitForElementByXpathAndSendKeys(
-                "//*[contains(@text,'Search Wikipedia')]",
+        waitForElementPresentAndSendKeys(
+                By.xpath("//*[contains(@text,'Search Wikipedia')]"),
                 "Java",
                 "Cannot find 'Search element' ",
                 5
@@ -134,12 +139,50 @@ public class SimpleTest {
         );
     }
 
+    @Test
+    public void swipeArticleTest() {
+
+        waitForElementPresentAndClick(
+                By.id("org.wikipedia:id/fragment_onboarding_skip_button"),
+                "Cannot find 'Skip element' ",
+                5
+        );
+
+        waitForElementPresentAndClick(
+                By.xpath("//*[contains(@text,'Search Wikipedia')]"),
+                "Cannot find 'Search element' ",
+                5
+        );
+
+        waitForElementPresentAndSendKeys(
+                By.xpath("//*[contains(@text,'Search Wikipedia')]"),
+                "Java",
+                "Cannot find 'Search element' ",
+                5
+        );
+
+        waitForWordPresent(
+                "//android.widget.TextView[@resource-id=\"org.wikipedia:id/page_list_item_title\" and contains(@text, 'Java')]",
+                "Cannot find 'elements with text Java' ",
+                5
+        );
+
+        verticalSwipeToBottom();
+        verticalSwipeToBottom();
+        verticalSwipeToBottom();
+
+    }
+
     private WebElement waitForElementPresent(By by, String error_message, long timeoutInSeconds) {
         WebDriverWait wait = new WebDriverWait(driver, timeoutInSeconds);
         wait.withMessage(error_message + "\n");
         return wait.until(
                 ExpectedConditions.presenceOfElementLocated(by)
         );
+    }
+    //перегрузка метода
+    private WebElement waitForElementPresent(By by, String error_message) {
+        return waitForElementPresent(by, error_message, 5);
     }
 
     private WebElement waitForElementPresentAndClick(By by, String error_message, long timeoutInSeconds) {
@@ -152,21 +195,6 @@ public class SimpleTest {
         WebElement element = waitForElementPresent(by, error_message, timeoutInSeconds);
         element.sendKeys(value);
         return element;
-    }
-
-    private WebElement waitForElementByIdAndClick(String id, String error_message, long timeoutInSeconds) {
-        By by = By.id(id);
-        return waitForElementPresentAndClick(by, error_message, timeoutInSeconds);
-    }
-
-    private WebElement waitForElementByXpathAndClick(String xpath, String error_message, long timeoutInSeconds) {
-        By by = By.xpath(xpath);
-        return waitForElementPresentAndClick(by, error_message, timeoutInSeconds);
-    }
-
-    private WebElement waitForElementByXpathAndSendKeys(String xpath, String value, String error_message, long timeoutInSeconds) {
-        By by = By.xpath(xpath);
-        return waitForElementPresentAndSendKeys(by, value, error_message, timeoutInSeconds);
     }
 
     private void assertElementHasText(String xpath, String expectedText, String errorMessage) {
@@ -193,6 +221,64 @@ public class SimpleTest {
         return wait.until(
                 ExpectedConditions.presenceOfElementLocated(by)
         );
+    }
+
+    protected void swipeUp(int timeOfSwipe) {
+
+        TouchAction touchAction = new TouchAction(driver);
+        Dimension size = driver.manage().window().getSize();
+
+        int x = size.width / 2;
+        int start_y = (int) (size.height * 0.8);
+        int end_y = (int) (size.height * 0.2);
+
+        touchAction.press(PointOption.point(x, start_y))
+                .waitAction(WaitOptions.waitOptions(Duration.ofMillis(timeOfSwipe)))
+                .moveTo(PointOption.point(x, end_y)).release().perform();
+    }
+
+    public void verticalSwipeToBottom() {
+        Dimension size = driver.manage().window().getSize();
+        int startY = (int) (size.height * 0.70);
+        int endY = (int) (size.height * 0.30);
+        int centerX = size.width / 2;
+
+        PointerInput finger = new PointerInput(PointerInput.Kind.TOUCH, "finger");
+        Sequence swipe = new Sequence(finger, 1);
+
+        //Дваигаем палец на начальную позицию
+        swipe.addAction(finger.createPointerMove(Duration.ofSeconds(0),
+                PointerInput.Origin.viewport(), centerX, (int) startY));
+        //Палец прикасакается к экрану
+        swipe.addAction(finger.createPointerDown(0));
+
+        //Палец двигается к конечной точке
+        swipe.addAction(finger.createPointerMove(Duration.ofMillis(700),
+                PointerInput.Origin.viewport(), centerX, (int) endY));
+
+        //Убираем палец с экрана
+        swipe.addAction(finger.createPointerUp(0));
+
+        //Выполняем действия
+        driver.perform(Arrays.asList(swipe));
+    }
+
+    protected void swipeUpQuick() {
+        swipeUp(200);
+    }
+
+    protected void swipeUpToFindElement(By by, String error_message, int max_swipes) {
+        int already_swiped = 0;
+        while (driver.findElements(by).size() == 0) {
+
+            if (already_swiped > max_swipes) {
+                waitForElementPresent(by, "Cannot find element by swipping up. \n" + error_message, 0);
+                return;
+            }
+            swipeUpQuick();
+            ++already_swiped;
+        }
+
     }
 
 }
