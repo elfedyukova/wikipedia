6538033ddd685032b414d91a4fe556e4a325f8a2
elfedyukova <elfedyukova@yandex.ru>
2024-04-27 07:04:00 +0300

    add ci/cd

---
+++ src/test/java/suites/TestSuite.java
@@ -0,0 +1,16 @@
+package suites;
+
+import org.example.*;
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+@RunWith(Suite.class)
+
+@Suite.SuiteClasses({
+        ArticleTests.class,
+        ChangeAppConditionTests.class,
+        SearchTests.class,
+        MyListsTests.class,
+        GetStartedTest.class
+})
+public class TestSuite {}
